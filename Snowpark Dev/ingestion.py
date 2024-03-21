2ae1bcb772735ce6c035ef2e3655efcaccc16b63
Taha Fanaswala <tahafanaswala@gmail.com>
2024-03-21 10:23:30 -0700

    Created new folder to write all Snowpark development code. In that folder, I added ingestion.py, which will be where we write code to ingest the Excel spreadsheets into Excel.

---
+++ Snowpark Dev/ingestion.py
@@ -0,0 +1,30 @@
+from snowflake.snowpark import Session
+import pandas as pd
+import json
+
+def create_session():
+    with open("creds.json", "r") as creds_file:
+        creds = json.load(creds_file)
+    
+    connection_params = {
+        "account": creds["account"],
+        "user": creds["uname"],
+        "password": creds["password"],
+        "role": "DATA_DEV",
+        "warehouse": "MY_WH"
+        }
+
+    session = Session.builder.configs(connection_params).create()
+    
+    return session
+    
+
+def write_location_data():
+    session = create_session()
+    session.use_schema("SNOWPARK_POC_DATA.PUBLIC")
+
+    raw_data = pd.read_excel("data/location.xlsx", sheet_name="location")
+    raw_data = session.createDataFrame(raw_data)
+    
+    raw_data.write.mode("overwrite").saveAsTable("location")
+
